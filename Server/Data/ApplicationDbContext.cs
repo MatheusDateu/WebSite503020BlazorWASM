178b9f3479fa1214c9d488bc96f9673aa0b8d766
MatheusDateu <mdelmondes5@gmail.com>
2023-06-15 03:16:07 -0400

    Adicionar arquivos de projeto.

---
+++ Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Duende.IdentityServer.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using WebSite503020BlazorWASM.Server.Models;
+
+namespace WebSite503020BlazorWASM.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
